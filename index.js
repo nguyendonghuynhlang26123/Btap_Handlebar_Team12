433c5db0beec530f89fdb4788fe6080274f710e5
nguyendonghuynhlang26123 <nguyendonghuynhlang@gmail.com>
2020-12-03 10:36:57 +0700

    Setup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 
 const hbs = require("express-handlebars");
+
 app.engine(
   "hbs",
   hbs({
@@ -12,10 +13,19 @@ app.engine(
     partialsDir: __dirname + "/views/partials/",
   })
 );
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "home" });
+});
+
+app.get("/task2", (req, res) => {
+  res.render("task3", { title: "Test" });
+});
+
+app.get("/task3", (req, res) => {
+  res.render("task3", { title: "Test" });
 });
 
 app.listen(port, () => {
